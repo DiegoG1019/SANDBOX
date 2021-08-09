eca2567f159c8a2b5d64a7232f88c9029cdecd2f
Diego García <dagarciam1014@gmail.com>
2021-08-09 02:11:57 -0400

    Toyed with RssFeedParser

---
+++ RssFeedParser-Test/RssFeedParser-Test/Program.cs
@@ -0,0 +1,24 @@
+﻿using RssFeedParser;
+using RssFeedParser.Models;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace RssFeedParser_Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var rssFeedParser = new FeedParser();
+            RssFeed feed = await rssFeedParser.ParseFeed("https://beta.erai-raws.info/feed/?res=720p&type=magnet&subs[]=us");
+
+            int i = 0;
+            P("Showing articles");
+            foreach (var x in feed.Articles)
+                P($"Article {++i}: *{x.Title}* \nPublished on {x.Published:G}\nCategories: {string.Join(", ", x.Categories.Select(y => y.Name))}\nLink: {x.Link}\n\n{x.Content}\n");
+
+            void P(object o) => Console.WriteLine(o);
+        }
+    }
+}
